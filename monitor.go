68819254e692b26f150f49cd228bb466506c6499
Johannes Hackl <johannes.hackl@live.com>
2026-03-13 15:53:24 +0100

    Fix: Reduced buckets and changed to seconds.

--- monitor.go
+++ monitor.go
@@ -26,7 +26,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "minitwit_http_request_duration_seconds",
 			Help:    "HTTP request duration in seconds",
-			Buckets: []float64{0.1, 0.25, 0.5, 1, 2.5, 5, 10, 25, 50, 100, 250, 500, 1000}, // Milliseconds
+			Buckets: []float64{0.01, 0.05, 0.25, 1, 5, 10}, // Seconds
 		},
 		[]string{"method", "path"},
 	)
@@ -39,7 +39,8 @@ func PrometheusMiddleware() gin.HandlerFunc {
 
 		c.Next() // execute the actual handler
 
-		duration := float64(time.Since(start).Microseconds() / 1000)
+		duration := time.Since(start).Seconds()
+
 		status := strconv.Itoa(c.Writer.Status())
 
 		// Use c.FullPath() instead of c.Request.URL.Path so that
